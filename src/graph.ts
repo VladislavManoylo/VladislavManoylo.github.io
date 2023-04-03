beb7625e39bdfc0b0f2ecd8225ee3ea0c6d07bcc
vlad <vladislav.manoylo@gmail.com>
2023-04-03 19:28:10 -0400

    popup buttons around nodes

--- src/graph.ts
+++ src/graph.ts
@@ -4,12 +4,23 @@ function overlap(p1: Point, p2: Point, r: number): Boolean {
   const dy = p1.y - p2.y;
   return dx * dx + dy * dy < r * r;
 }
+function polarToPoint(a: number, r: number) {
+  return { x: r * Math.cos(a), y: r * Math.sin(a) };
+}
+function addPoint(p1: Point, p2: Point) {
+  return { x: p1.x + p2.x, y: p1.y + p2.y };
+}
+function subPoint(p1: Point, p2: Point) {
+  return { x: p1.x - p2.x, y: p1.y - p2.y };
+}
+
+const radius = 50;
+const buttonRadius = 20;
 
 class Drawer {
   canvas: HTMLCanvasElement;
   ctx: CanvasRenderingContext2D;
   prev: Point = { x: 0, y: 0 };
-  radius = 40;
   nodes: Point[] = [];
   constructor() {
     this.canvas = document.getElementById("graph-canvas") as HTMLCanvasElement;
@@ -20,18 +31,16 @@ class Drawer {
 
   addNode(point: Point): void {
     this.nodes.push(point);
-    this.draw(false)
   }
 
   deleteNode(i: number): void {
     this.nodes.splice(i, 1);
-    this.draw()
   }
 
   /** returns the highest index node that overlaps with the point */
   nodeAt(point: Point): number | undefined {
     for (let i = this.nodes.length - 1; i >= 0; i--) {
-      if (overlap(point, this.nodes[i], this.radius)) {
+      if (overlap(point, this.nodes[i], radius)) {
         return i;
       }
     }
@@ -57,19 +66,16 @@ class Drawer {
   drawEdge(i: number, j: number): void {
     let [x1, y1, x2, y2]: number[] = [this.nodes[i].x, this.nodes[i].y, this.nodes[j].x, this.nodes[j].y];
     let angle = Math.atan2(y2 - y1, x2 - x1);
-    let dx = this.radius * Math.cos(angle);
-    let dy = this.radius * Math.sin(angle);
-    x1 += dx;
-    x2 -= dx;
-    y1 += dy;
-    y2 -= dy;
-    this.ctx.moveTo(x1, y1);
-    this.ctx.lineTo(x2, y2);
+    let offset = polarToPoint(angle, radius);
+    let p1 = addPoint(this.nodes[i], offset);
+    let p2 = subPoint(this.nodes[j], offset);
+    this.ctx.moveTo(p1.x, p1.y);
+    this.ctx.lineTo(p2.x, p2.y);
     this.ctx.stroke();
   }
 
   /** draws a labeled circle, centered at a point, with a label */
-  drawCircle(p: Point, label: string, r: number = this.radius): void {
+  drawCircle(p: Point, label: string, r: number = radius): void {
     // circle
     this.ctx.beginPath();
     this.ctx.arc(p.x, p.y, r, 0, 2 * Math.PI);
@@ -84,16 +90,72 @@ class Drawer {
   }
 }
 
-const drawer = new Drawer();
-drawer.canvas.addEventListener("click", (event) => {
-  const x = event.clientX - drawer.canvas.offsetLeft;
-  const y = event.clientY - drawer.canvas.offsetTop;
-  const i = drawer.nodeAt({ x, y });
-  if (i === undefined) {
-    drawer.addNode({ x, y });
+class Buttons {
+  ctx: CanvasRenderingContext2D;
+  buttons: Point[];
+  selected: number; // the selected node in the graph
+  constructor(ctx: CanvasRenderingContext2D, pos: Point, selected: number) {
+    this.ctx = ctx;
+    this.selected = selected;
+    this.buttons = [];
+    for (const a of [-1 / 6, -1 / 2, -5 / 6]) {
+      this.buttons.push(addPoint(pos, polarToPoint(a * Math.PI, radius)))
+    }
   }
-  else {
-    drawer.deleteNode(i);
+  buttonAt(pos: Point): number | undefined {
+    for (let i = 0; i < this.buttons.length; i++) {
+      if (overlap(pos, this.buttons[i], buttonRadius)) {
+        return i;
+      }
+    }
+    return undefined;
   }
-});
+  draw(): void {
+    for (const it of this.buttons) {
+      this.ctx.beginPath();
+      this.ctx.arc(it.x, it.y, buttonRadius, 0, 2 * Math.PI);
+      this.ctx.fillStyle = "red";
+      this.ctx.fill();
+      this.ctx.stroke();
+    }
+  }
+}
 
+class Controller {
+  buttons: Buttons | undefined = undefined;
+  drawer: Drawer = new Drawer();
+  constructor() {
+    this.drawer.canvas.addEventListener("click", (event) => {
+      const x = event.clientX - this.drawer.canvas.offsetLeft;
+      const y = event.clientY - this.drawer.canvas.offsetTop;
+      this.click({ x, y });
+    });
+  }
+  click(pos: Point): void {
+    let i = this.buttons?.buttonAt(pos)
+    if (i !== undefined) {
+      console.log('a');
+      this.drawer.deleteNode(this.buttons!.selected);
+      this.drawer.draw();
+      this.buttons = undefined;
+      return;
+    }
+    i = this.drawer.nodeAt(pos);
+    if (i !== undefined) {
+      console.log('b')
+      this.buttons = new Buttons(this.drawer.ctx, this.drawer.nodes[i], i);
+      this.drawer.draw();
+      this.buttons.draw();
+      return;
+    }
+    else {
+      console.log('c');
+      this.drawer.addNode(pos);
+      this.buttons = undefined;
+      this.drawer.draw();
+      return;
+    }
+  }
+}
+
+new Controller();
