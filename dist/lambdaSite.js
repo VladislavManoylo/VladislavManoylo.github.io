1f2880f11936b5ef7d2b9ff852179f708dfa8f37
vlad <vladislav.manoylo@gmail.com>
2023-05-19 20:34:07 -0400

    Remove env parsing for now

--- dist/lambdaSite.js
+++ dist/lambdaSite.js
@@ -1,50 +1,38 @@
-import { toSexpr } from "./sexpr.js";
-import { toLambdaExpr, exprString, evalLambda, } from "./lambda.js";
-function readEnv(str) {
-    let ret = {};
-    let s = toSexpr(str);
-    for (let i = 0; i < s.length; i += 2) {
-        ret[s[i]] = toLambdaExpr(s[i + 1]);
-    }
-    return ret;
-}
-function readExpr(str) {
-    return toLambdaExpr(toSexpr(str)[0]);
-}
-let envStr = `ID (lambda (x) x)
-T (lambda (x y) x)
-F (lambda (x y) y)
-NOT (lambda (p) (p F T))
-AND (lambda (p q) (p q p))
-OR (lambda (p q) (p p q))
-0 (lambda (f x) x)
-SUCC (lambda (n f x) (f (n f x)))
-PLUS (lambda (m n) (m SUCC n))
-MULT (lambda (m n) (m (plus n) 0))
-POW (lambda (b e) (e b))
-1 (lambda (f x) (f x))
-2 (lambda (f x) (f (f x)))
-3 (SUCC 2)
-4 (SUCC 3)
-`;
-let exprStr = `SUCC 0`;
-let env = readEnv(envStr);
-let expr = readExpr(exprStr);
-let output = evalLambda(expr, env);
-let envText = document.getElementById("env");
+import { evalLambda, read, format, } from "./lambda.js";
+// let envStr = `ID (lambda (x) x)
+// T (lambda (x y) x)
+// F (lambda (x y) y)
+// NOT (lambda (p) (p F T))
+// AND (lambda (p q) (p q p))
+// OR (lambda (p q) (p p q))
+// 0 (lambda (f x) x)
+// SUCC (lambda (n f x) (f (n f x)))
+// PLUS (lambda (m n) (m SUCC n))
+// MULT (lambda (m n) (m (plus n) 0))
+// POW (lambda (b e) (e b))
+// 1 (lambda (f x) (f x))
+// 2 (lambda (f x) (f (f x)))
+// 3 (SUCC 2)
+// 4 (SUCC 3)
+// `;
+let exprStr = `(lambda (n f x) f (n f x)) (lambda (f x) f x)`;
+// let env: Env = readEnv(envStr);
+let expr = read(exprStr);
+let output = evalLambda(expr);
+// let envText = document.getElementById("env") as HTMLTextAreaElement;
 let outputText = document.getElementById("output");
 let exprText = document.getElementById("input");
-envText.textContent = envStr;
+// envText.textContent = envStr;
 exprText.textContent = exprStr;
-outputText.textContent = exprString(expr) + "\n" + exprString(evalLambda(output));
-envText.addEventListener("input", (event) => {
-    let k = event.target.value;
-    env = readEnv(k);
-});
+outputText.textContent = format(expr) + "\n" + format(evalLambda(output));
+// envText.addEventListener("input", (event) => {
+//   let k: string = (event.target as HTMLInputElement).value;
+//   env = readEnv(k);
+// });
 exprText.addEventListener("input", (event) => {
     let k = event.target.value;
-    expr = readExpr(k);
-    outputText.textContent += exprString(expr);
-    output = evalLambda(expr, env);
-    outputText.textContent += "\n" + exprString(output);
+    expr = read(k);
+    outputText.textContent += format(expr);
+    output = evalLambda(expr);
+    outputText.textContent += "\n" + format(output);
 });
--- src/lambdaSite.ts
+++ src/lambdaSite.ts
@@ -1,63 +1,51 @@
 import { sexpr, toSexpr } from "./sexpr.js";
 import {
   LambdaExpr,
-  Env,
-  toLambdaExpr,
-  exprString,
   evalLambda,
+  read,
+  format,
 } from "./lambda.js";
 
-function readEnv(str: string): Env {
-  let ret: Env = {};
-  let s: sexpr[] = toSexpr(str) as sexpr[];
-  for (let i = 0; i < s.length; i += 2) {
-    ret[s[i] as string] = toLambdaExpr(s[i + 1]);
-  }
-  return ret;
-}
+// let envStr = `ID (lambda (x) x)
+// T (lambda (x y) x)
+// F (lambda (x y) y)
+// NOT (lambda (p) (p F T))
+// AND (lambda (p q) (p q p))
+// OR (lambda (p q) (p p q))
+// 0 (lambda (f x) x)
+// SUCC (lambda (n f x) (f (n f x)))
+// PLUS (lambda (m n) (m SUCC n))
+// MULT (lambda (m n) (m (plus n) 0))
+// POW (lambda (b e) (e b))
+// 1 (lambda (f x) (f x))
+// 2 (lambda (f x) (f (f x)))
+// 3 (SUCC 2)
+// 4 (SUCC 3)
+// `;
 
-function readExpr(str: string): LambdaExpr {
-  return toLambdaExpr(toSexpr(str)[0]);
-}
+let exprStr = `(lambda (n f x) f (n f x)) (lambda (f x) f x)`;
 
-let envStr = `ID (lambda (x) x)
-T (lambda (x y) x)
-F (lambda (x y) y)
-NOT (lambda (p) (p F T))
-AND (lambda (p q) (p q p))
-OR (lambda (p q) (p p q))
-0 (lambda (f x) x)
-SUCC (lambda (n f x) (f (n f x)))
-PLUS (lambda (m n) (m SUCC n))
-MULT (lambda (m n) (m (plus n) 0))
-POW (lambda (b e) (e b))
-1 (lambda (f x) (f x))
-2 (lambda (f x) (f (f x)))
-3 (SUCC 2)
-4 (SUCC 3)
-`;
-let exprStr = `SUCC 0`;
+// let env: Env = readEnv(envStr);
+let expr: LambdaExpr = read(exprStr);
+let output: LambdaExpr = evalLambda(expr);
 
-let env: Env = readEnv(envStr);
-let expr: LambdaExpr = readExpr(exprStr);
-let output: LambdaExpr = evalLambda(expr, env);
-
-let envText = document.getElementById("env") as HTMLTextAreaElement;
+// let envText = document.getElementById("env") as HTMLTextAreaElement;
 let outputText = document.getElementById("output") as HTMLTextAreaElement;
 let exprText = document.getElementById("input") as HTMLTextAreaElement;
 
-envText.textContent = envStr;
+// envText.textContent = envStr;
 exprText.textContent = exprStr;
-outputText.textContent = exprString(expr) + "\n" + exprString(evalLambda(output));
+outputText.textContent = format(expr) + "\n" + format(evalLambda(output));
 
-envText.addEventListener("input", (event) => {
-  let k: string = (event.target as HTMLInputElement).value;
-  env = readEnv(k);
-});
+// envText.addEventListener("input", (event) => {
+//   let k: string = (event.target as HTMLInputElement).value;
+//   env = readEnv(k);
+// });
+
 exprText.addEventListener("input", (event) => {
   let k: string = (event.target as HTMLInputElement).value;
-  expr = readExpr(k);
-  outputText.textContent += exprString(expr);
-  output = evalLambda(expr, env);
-  outputText.textContent += "\n" + exprString(output);
+  expr = read(k);
+  outputText.textContent += format(expr);
+  output = evalLambda(expr);
+  outputText.textContent += "\n" + format(output);
 });
