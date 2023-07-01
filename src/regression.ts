ac398a01ce0fda6af1bbc3359ae4fb4887533ae9
vlad <vladislav.manoylo@gmail.com>
2023-06-30 23:10:46 -0700

    Store points as plot coordinates

--- src/regression.ts
+++ src/regression.ts
@@ -8,7 +8,7 @@ let x1 = 1;
 let y0 = 0;
 let y1 = 1;
 let margin = 50;
-
+let plotSize = [canvas.width - margin, canvas.height - margin];
 let points: v2[] = [];
 
 function redraw() {
@@ -16,18 +16,20 @@ function redraw() {
   {
     // axes
     let left = margin - 5;
-    let bottom = canvas.height - margin;
     pencil.text(y1.toFixed(0), [left, 20], "1em Arial", "right");
-    pencil.text(y0.toFixed(0), [left, bottom], "1em Arial", "right");
-    bottom += 20;
+    pencil.text(y0.toFixed(0), [left, plotSize[1]], "1em Arial", "right");
+    let bottom = plotSize[1] + 20;
     pencil.text(x0.toFixed(0), [left, bottom], "1em Arial", "left");
     pencil.text(x1.toFixed(0), [canvas.width, bottom], "1em Arial", "right");
   }
-  pencil.ctx.translate(margin, canvas.height - margin);
+  pencil.ctx.translate(margin, plotSize[1]);
   pencil.ctx.scale(1, -1);
-  pencil.ctx.strokeRect(0, 0, canvas.width - margin, canvas.height - margin);
+  pencil.ctx.strokeRect(0, 0, plotSize[0], plotSize[1]);
   // points
-  for (let it of points) pencil.ctx.fillRect(it[0], it[1], 1, 1);
+  for (let it of points) {
+    console.log("point", it[0], it[1]);
+    pencil.ctx.fillRect(it[0] * plotSize[0], it[1] * plotSize[1], 1, 1);
+  }
 }
 redraw();
 
@@ -36,7 +38,7 @@ canvas.addEventListener("click", (event) => {
   let x = event.x - margin;
   let y = canvas.height - margin - event.y;
   if (x > 0 && y > 0) {
-    points.push([x, y]);
+    points.push([x / plotSize[0], y / plotSize[1]]);
+    redraw();
   }
-  redraw();
 });
