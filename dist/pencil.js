8401fb92bbbf5d1e1b83cd337856b44bfe3f7f04
vlad <vladislav.manoylo@gmail.com>
2023-06-30 22:17:37 -0700

    Remove local canvas in pencil

--- dist/pencil.js
+++ dist/pencil.js
@@ -8,11 +8,10 @@ export function polarToPoint(a, r) {
 }
 export class Pencil {
     constructor(canvas) {
-        this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
     }
     clear() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
     }
     line(a, b) {
         this.ctx.beginPath();
--- src/pencil.ts
+++ src/pencil.ts
@@ -11,14 +11,12 @@ export function polarToPoint(a: number, r: number): v2 {
 }
 
 export class Pencil {
-  private canvas: HTMLCanvasElement;
-  private ctx: CanvasRenderingContext2D;
+  ctx: CanvasRenderingContext2D;
   constructor(canvas: HTMLCanvasElement) {
-    this.canvas = canvas;
     this.ctx = canvas.getContext("2d")!;
   }
   clear() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
   }
   line(a: v2, b: v2) {
     this.ctx.beginPath();
