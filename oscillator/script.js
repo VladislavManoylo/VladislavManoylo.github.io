bc5f220ce58ce28060f3ba50eb55d5e929752f70
vlad <vladislav.manoylo@gmail.com>
2024-11-18 11:18:13 -0500

    create nodes in script

--- oscillator/script.js
+++ oscillator/script.js
@@ -6,6 +6,20 @@ const hzDials = document.getElementsByClassName("hz");
 const audioCtx = new AudioContext();
 let nodes = [null, null];
 
+function makeNodeDiv(i) {
+    return `
+    <div>
+        <label for="phase${i}">phase
+            <input id="phase${i}" class="phase" type="range" min="0" max="1" step="0.1" onchange="change()">
+        </label>
+        <label for="hz${i}">hz
+            <input id="hz${i}" class="hz" type="number" value="440" onchange="change()">
+        </label>
+    </div>
+        `
+}
+document.body.innerHTML = makeNodeDiv(1) + makeNodeDiv(2);
+
 function synctime(phase, hz) {
     const p = 1 / hz;
     const t = audioCtx.currentTime + 0.1; // extra time to get sync correct
