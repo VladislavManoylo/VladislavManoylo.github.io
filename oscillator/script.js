2fcb044ae6558583559efd8ce079084896be72c5
vlad <vladislav.manoylo@gmail.com>
2024-11-18 11:47:41 -0500

    add and remove multiple nodes

--- oscillator/script.js
+++ oscillator/script.js
@@ -1,10 +1,8 @@
-/** @type{HTMLInputElement[]} */
-const phaseDials = document.getElementsByClassName("phase");
-/** @type{HTMLInputElement[]} */
-const hzDials = document.getElementsByClassName("hz");
-
 const audioCtx = new AudioContext();
-let nodes = [null, null];
+/** @type{HTMLDivElement} */
+const nodeContainer = document.getElementById("nodes");
+/** @type{OscillatorNode[]} */
+const nodes = [];
 
 function makeNodeDiv(i) {
     return `
@@ -15,32 +13,46 @@ function makeNodeDiv(i) {
         <label for="hz${i}">hz
             <input id="hz${i}" class="hz" type="number" value="440" onchange="change()">
         </label>
+        <button class="x" type="button" onclick="rmNode(${i})">x</button>
     </div>
         `
 }
-document.body.innerHTML = makeNodeDiv(1) + makeNodeDiv(2);
+
+function addNode() {
+    nodeContainer.insertAdjacentHTML("beforeend", makeNodeDiv(nodes.length));
+    nodes.push(makeNode());
+    change();
+}
+
+function rmNode(i) {
+    console.log(i, nodes);
+    nodeContainer.removeChild(nodeContainer.children[i]);
+    nodes[i].stop();
+    nodes.splice(i, 1);
+}
 
 function synctime(phase, hz) {
     const p = 1 / hz;
-    const t = audioCtx.currentTime + 0.1; // extra time to get sync correct
+    const t = audioCtx.currentTime + 0.2; // extra time to get sync correct
     return t - (t % p) + phase * p;
 }
 
-function makeNode(hz = 440) {
+function makeNode(phase = 0, hz = 440) {
     const node = audioCtx.createOscillator();
     node.frequency.value = hz;
     node.connect(audioCtx.destination);
+    node.start(synctime(phase, hz));
     return node;
 }
 
 function change() {
-    for (let i = 0; i < 2; i++) {
-        if (nodes[i] !== null)
-            nodes[i].stop();
+    const phaseDials = document.getElementsByClassName("phase");
+    const hzDials = document.getElementsByClassName("hz");
+    for (let i = 0; i < nodes.length; i++) {
+        nodes[i].stop();
         const phase = phaseDials[i].value;
         const hz = hzDials[i].value;
         console.log(i, phase, hz);
-        nodes[i] = makeNode(hz);
-        nodes[i].start(synctime(phase, hz));
+        nodes[i] = makeNode(phase, hz);
     }
 }
