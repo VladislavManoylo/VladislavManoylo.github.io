cd5a0417421f7f7b248d990f0c70f0ab5256c0a3
vlad <vladislav.manoylo@gmail.com>
2024-11-18 12:12:01 -0500

    detune dial

--- oscillator/script.js
+++ oscillator/script.js
@@ -7,16 +7,16 @@ const nodes = [];
 function makeNodeDiv(i) {
     return `
     <div>
-        <label for="phase${i}">phase
+        <label for="phase${i}" title="set phase of wave- two identical waves with opposite phase wil cancel out">phase
             <input id="phase${i}" class="phase" type="range" min="0" max="1" step="0.1" onchange="change()">
         </label>
-        <label for="hz${i}">hz
+        <label for="hz${i}" title="frequency of note">hz
             <input id="hz${i}" class="hz" type="number" value="440" onchange="change()">
         </label>
-        <label for="detune${i}">detune
+        <label for="detune${i}" title="detune note by cents (100 cents per 12 EDO semitone)">detune
             <input id="detune${i}" class="detune" type="number" value="0" onchange="change()">
         </label>
-        <button class="x" type="button" onclick="rmNode(${i})">x</button>
+        <button class="x" type="button" onclick="rmNode(${i})" title="remove note">x</button>
     </div>
         `
 }
