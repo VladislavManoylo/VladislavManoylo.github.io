412b0bade3db1f2b8903128fe9b2f7098de48c92
vlad <vladislav.manoylo@gmail.com>
2024-11-19 15:59:20 -0500

    plot waves and fix input parsing

--- oscillator/script.js
+++ oscillator/script.js
@@ -1,5 +1,10 @@
 const detuned = (hz, detune) => hz * Math.pow(2, detune / 1200);
 const nums = (s) => s.split(",").map(Number);
+function sumFunctions(functions) {
+    return function(...args) {
+        return functions.reduce((sum, func) => sum + func(...args), 0);
+    };
+}
 function tablerow(...cells) {
     let ret = "<tr>"
     for (const x of cells) {
@@ -10,33 +15,80 @@ function tablerow(...cells) {
 }
 
 /** @typedef {"sine" | "square" | "triangle" | "sawtooth" | "custom" } wavetype */
-/** @typedef {Object} note
- *  @property {number} phase
- *  @property {number} hz
- *  @property {number} detune
- *  @property {wavetype} wavetype
- *  @property {number[]} real - coefficients for custom wavetype
- *  @property {number[]} imag - coefficients for custom wavetype
- * */
 
-/** @type{note} */
-const defaultNote = {
-    phase: 0,
-    hz: 440,
-    detune: 0,
-    wavetype: "sine",
-    real: [0, 0],
-    imag: [0, 1],
-};
+class Note {
+    constructor() {
+        this.phase = 0;
+        this.hz = 440;
+        this.detune = 0;
+        this.wavetype = "sine";
+        this.real = [0, 0];
+        this.imag = [0, 1];
+    }
+
+    frequency() {
+        return detuned(this.hz, this.detune);
+    }
+
+    makeDiv() {
+        const ret = document.createElement("div");
+        ret.classList.add("note");
+        ret.innerHTML =
+            '<button class="x" type="button" onclick="rmNote(this.parentElement)" title="remove note">x</button>' +
+            labeledInput("phase", this.phase, "set phase of wave- two identical waves with opposite phase wil cancel out", 'type="range" min="0" max="1" step="0.1"') +
+            labeledInput("hz", this.hz, "frequency of note", 'type="number"') +
+            labeledInput("detune", this.detune, "detune note by cents (100 cents per 12 EDO semitone)", 'type="number"');
+        const wavetype = makeRadioDiv(`wavetype`, ["sine", "triangle", "square", "sawtooth", "custom"]);
+        wavetype.innerHTML += `
+        <div>
+        ${labeledInput('real', this.real, 'real coefficients', 'type="text"')}
+        ${labeledInput('imag', this.imag, 'imaginary coefficients', 'type="text"')}
+        </div>
+        `
+        ret.appendChild(wavetype);
+        return ret;
+    }
+
+    makePlayer() {
+        const ret = audioCtx.createOscillator();
+        ret.frequency.value = this.hz;
+        ret.detune.value = this.detune;
+        if (this.wavetype == "custom") {
+            let r = this.real;
+            let i = this.imag;
+            const dl = r.length - i.length;
+            if (dl > 0) {
+                i = [...i, ...Array(dl).fill(0)];
+            } else if (dl < 0) {
+                r = [...r, ...Array(-dl).fill(0)];
+            }
+            ret.setPeriodicWave(audioCtx.createPeriodicWave(r, i));
+        } else {
+            ret.type = this.wavetype;
+        }
+        ret.connect(audioCtx.destination);
+        ret.start(synctime(this.phase, this.hz));
+        return ret;
+    }
+
+    getFunc() {
+        const p = this.phase / this.frequency();
+        // console.log("phase", this.phase, this.frequency());
+        return (t) => Math.sin(2 * Math.PI * this.frequency() * (t - p));
+    }
+}
 
 const audioCtx = new AudioContext();
 /** @type{HTMLDivElement} */
 const noteContainer = document.getElementById("notes");
 /** @type{HTMLTableElement} */
 const wavetableContainer = document.getElementById("table");
+/** @type{HTMLCanvasElement} */
+const canvas = document.getElementById("plot");
+const ctx = canvas.getContext("2d");
 /** @type{OscillatorNode[]} */
 const notePlayers = [];
-/** @type{note[]} */
+/** @type{Note[]} */
 const notes = []
 
 /**
@@ -74,45 +126,17 @@ function makeRadioDiv(name, values) {
     return ret;
 }
 
-/**
- * note div with values set
- *
- * @param {note} note
- * @returns {HTMLDivElement}
- */
-function makeNoteDiv(note) {
-    const ret = document.createElement("div");
-    ret.classList.add("note");
-    makeNoteDiv.i = (makeNoteDiv.i || 0) + 1;
-    i = makeNoteDiv.i;
-    ret.innerHTML =
-        '<button class="x" type="button" onclick="rmNote(this.parentElement)" title="remove note">x</button>' +
-        labeledInput("phase", note.phase, "set phase of wave- two identical waves with opposite phase wil cancel out", 'type="range" min="0" max="1" step="0.1"') +
-        labeledInput("hz", note.hz, "frequency of note", 'type="number"') +
-        labeledInput("detune", note.detune, "detune note by cents (100 cents per 12 EDO semitone)", 'type="number"');
-
-    const wavetype = makeRadioDiv(`wavetype`, ["sine", "triangle", "square", "sawtooth", "custom"]);
-    wavetype.innerHTML += `
-        <div>
-        ${labeledInput('real', note.real, 'real coefficients', 'type="text"')}
-        ${labeledInput('imag', note.imag, 'imaginary coefficients', 'type="text"')}
-        </div>
-        `
-    ret.appendChild(wavetype);
-    return ret;
-}
-
 function addNote() {
-    const n = structuredClone(defaultNote);
-    noteContainer.appendChild(makeNoteDiv(n));
-    notePlayers.push(makeNotePlayer(n));
+    const n = new Note();
+    noteContainer.appendChild(n.makeDiv());
+    notePlayers.push(n.makePlayer());
     notes.push(n);
     display();
 }
 
 function rmNote(div) {
     const i = [...noteContainer.children].indexOf(div);
-    console.log("rm", i, div, notePlayers);
+    // console.log("rm", i, div, notePlayers);
     notePlayers[i].stop();
     notePlayers.splice(i, 1);
     notes.splice(i, 1);
@@ -124,26 +148,6 @@ function synctime(phase, hz) {
     const p = 1 / hz;
     const t = audioCtx.currentTime + 0.2; // extra time to get sync correct
     return t - (t % p) + phase * p;
-}
-
-/**
- * start an oscillator in the audio context
- *
- * @param {note} note
- * @returns {OscillatorNode}
- */
-function makeNotePlayer(note) {
-    const ret = audioCtx.createOscillator();
-    ret.frequency.value = note.hz;
-    ret.detune.value = note.detune;
-    if (note.wavetype == "custom") {
-        ret.setPeriodicWave(audioCtx.createPeriodicWave(note.real, note.imag));
-    } else {
-        ret.type = note.wavetype;
-    }
-    ret.connect(audioCtx.destination);
-    ret.start(synctime(note.phase, note.hz));
-    return ret;
 }
 
 /**
@@ -176,28 +180,52 @@ function change(div, cls, val) {
     if (i === null) {
         return
     }
-    notes[i][cls] = val;
-    console.log("change", i, cls, val);
     switch (cls) {
-        case "phase": notes[i][cls] = parseInt(val); break;
-        case "hz": notes[i][cls] = parseInt(val); break;
-        case "detune": notes[i][cls] = parseInt(val); break;
+        case "phase": notes[i][cls] = parseFloat(val); break;
+        case "hz": notes[i][cls] = parseFloat(val); break;
+        case "detune": notes[i][cls] = parseFloat(val); break;
         case "wavetype": notes[i][cls] = val; break;
         case "real": notes[i][cls] = nums(val); break;
         case "imag": notes[i][cls] = nums(val); break;
     }
+    console.log("change", i, cls, val, notes[i]);
     notePlayers[i].stop()
-    notePlayers[i] = makeNotePlayer(notes[i]);
+    notePlayers[i] = notes[i].makePlayer();
     display();
+}
+
+/**
+ * plot a function on the canvas
+ *
+ * @param {Function} f - function to plot
+ * @param {[number, number]} [xrange] - range of x values to plot
+ * @param {[number, number]} [yrange] - range of y values to plot
+ */
+function plotfunc(f, xrange, yrange) {
+    const xscale = canvas.width / (xrange[1] - xrange[0]);
+    const yscale = canvas.height / (yrange[1] - yrange[0]);
+    const cf = (x) => (f(x / xscale + xrange[0]) - yrange[0]) * yscale;
+    ctx.beginPath();
+    ctx.moveTo(0, cf(0));
+    for (let x = 1; x < canvas.width; x++) {
+        ctx.lineTo(x, cf(x));
+    }
+    ctx.stroke();
 }
 
 
 function display() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     let html = "<thead><th>frequency</th><th>phase</th></thead><tbody>";
+    const funcs = [];
+    const frequencies = [];
     for (let i = 0; i < notes.length; i++) {
-        const f = detuned(notes[i].hz, notes[i].detune);
-        html += tablerow(f, notes[i].phase);
+        frequencies.push(notes[i].frequency());
+        funcs.push(notes[i].getFunc());
+        plotfunc(funcs[i], [0, 1 / 440], [-2, 2]);
+        html += tablerow(frequencies[i], notes[i].phase);
     }
+    plotfunc(sumFunctions(funcs), [0, 1 / 440], [-2, 2]);
     html += "</tbody>";
     wavetableContainer.innerHTML = html
 }
