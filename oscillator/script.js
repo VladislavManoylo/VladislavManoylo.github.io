d2edfa56c8ea9696e5baca1bb27d2d7108df5e42
vlad <vladislav.manoylo@gmail.com>
2024-11-19 12:20:23 -0500

    rename node -> note

--- oscillator/script.js
+++ oscillator/script.js
@@ -1,5 +1,5 @@
 /** @typedef {"sine" | "square" | "triangle" | "sawtooth" | "custom" } wavetype */
-/** @typedef {Object} node
+/** @typedef {Object} note
  *  @property {number} phase
  *  @property {number} hz
  *  @property {number} detune
@@ -8,8 +8,8 @@
  *  @property {number[]} imag - coefficients for custom wavetype
  * */
 
-/** @type{node} */
-const defaultNode = {
+/** @type{note} */
+const defaultNote = {
     phase: 0,
     hz: 440,
     detune: 0,
@@ -18,15 +18,15 @@ const defaultNode = {
     imag: [0, 1],
 };
 
-const nums = (s) => s.split(/ +/).map(Number);
+const nums = (s) => s.split(",").map(Number);
 
 const audioCtx = new AudioContext();
 /** @type{HTMLDivElement} */
-const nodeContainer = document.getElementById("nodes");
+const noteContainer = document.getElementById("notes");
 /** @type{OscillatorNode[]} */
-const nodes = [];
-/** @type{node[]} */
-const nodeVals = []
+const notePlayers = [];
+/** @type{note[]} */
+const notes = []
 
 /**
  * create input with a label
@@ -64,47 +64,47 @@ function makeRadioDiv(name, values) {
 }
 
 /**
- * node div with values set
+ * note div with values set
  *
- * @param {node} node
+ * @param {note} note
  * @returns {HTMLDivElement}
  */
-function makeNodeDiv(node) {
+function makeNoteDiv(note) {
     const ret = document.createElement("div");
     ret.classList.add("note");
-    makeNodeDiv.i = (makeNodeDiv.i || 0) + 1;
-    i = makeNodeDiv.i;
+    makeNoteDiv.i = (makeNoteDiv.i || 0) + 1;
+    i = makeNoteDiv.i;
     ret.innerHTML =
         '<button class="x" type="button" onclick="rmNode(this.parentElement)" title="remove note">x</button>' +
-        labeledInput("phase", node.phase, "set phase of wave- two identical waves with opposite phase wil cancel out", 'type="range" min="0" max="1" step="0.1"') +
-        labeledInput("hz", node.hz, "frequency of note", 'type="number"') +
-        labeledInput("detune", node.detune, "detune note by cents (100 cents per 12 EDO semitone)", 'type="number"');
+        labeledInput("phase", note.phase, "set phase of wave- two identical waves with opposite phase wil cancel out", 'type="range" min="0" max="1" step="0.1"') +
+        labeledInput("hz", note.hz, "frequency of note", 'type="number"') +
+        labeledInput("detune", note.detune, "detune note by cents (100 cents per 12 EDO semitone)", 'type="number"');
 
     const wavetype = makeRadioDiv(`wavetype`, ["sine", "triangle", "square", "sawtooth", "custom"]);
     wavetype.innerHTML += `
         <div>
-        ${labeledInput('real', node.real, 'real coefficients', 'type="text"')}
-        ${labeledInput('imag', node.imag, 'imaginary coefficients', 'type="text"')}
+        ${labeledInput('real', note.real, 'real coefficients', 'type="text"')}
+        ${labeledInput('imag', note.imag, 'imaginary coefficients', 'type="text"')}
         </div>
         `
     ret.appendChild(wavetype);
     return ret;
 }
 
-function addNode() {
-    const n = structuredClone(defaultNode);
-    nodeContainer.appendChild(makeNodeDiv(n));
-    nodes.push(makeNodePlayer(n));
-    nodeVals.push(n);
+function addNote() {
+    const n = structuredClone(defaultNote);
+    noteContainer.appendChild(makeNoteDiv(n));
+    notePlayers.push(makeNotePlayer(n));
+    notes.push(n);
 }
 
-function rmNode(div) {
-    const i = [...nodeContainer.children].indexOf(div);
-    console.log("rm", i, div, nodes);
-    nodes[i].stop();
-    nodes.splice(i, 1);
-    nodeVals.splice(i, 1);
-    nodeContainer.removeChild(div);
+function rmNote(div) {
+    const i = [...noteContainer.children].indexOf(div);
+    console.log("rm", i, div, notePlayers);
+    notePlayers[i].stop();
+    notePlayers.splice(i, 1);
+    notes.splice(i, 1);
+    noteContainer.removeChild(div);
 }
 
 function synctime(phase, hz) {
@@ -116,30 +116,30 @@ function synctime(phase, hz) {
 /**
  * start an oscillator in the audio context
  *
- * @param {node} vals
+ * @param {note} note
  * @returns {OscillatorNode}
  */
-function makeNodePlayer(vals) {
-    const node = audioCtx.createOscillator();
-    node.frequency.value = vals.hz;
-    node.detune.value = vals.detune;
-    if (vals.type == "custom") {
-        node.setPeriodicWave(audioCtx.createPeriodicWave(vals.real, vals.imag));
+function makeNotePlayer(note) {
+    const ret = audioCtx.createOscillator();
+    ret.frequency.value = note.hz;
+    ret.detune.value = note.detune;
+    if (note.type == "custom") {
+        ret.setPeriodicWave(audioCtx.createPeriodicWave(note.real, note.imag));
     } else {
-        node.type = vals.wavetype;
+        ret.type = note.wavetype;
     }
-    node.connect(audioCtx.destination);
-    node.start(synctime(vals.phase, vals.hz));
-    return node;
+    ret.connect(audioCtx.destination);
+    ret.start(synctime(note.phase, note.hz));
+    return ret;
 }
 
 /**
- * node index that the div is inside of
+ * position inside the note container
  *
  * @param {HTMLElement} div
  * @returns {number | null}
  */
-function nodeIndex(div) {
+function containerIndex(div) {
     let note = div;
     while (note !== null && !note.classList.contains("note")) {
         note = note.parentElement;
@@ -159,20 +159,20 @@ function nodeIndex(div) {
  * @param {string} val - value
  */
 function change(div, cls, val) {
-    const i = nodeIndex(div);
+    const i = containerIndex(div);
     if (i === null) {
         return
     }
-    nodeVals[i][cls] = val;
+    notes[i][cls] = val;
     console.log("change", i, cls, val);
     switch (cls) {
-        case "phase": nodeVals[i][cls] = parseInt(val); break;
-        case "hz": nodeVals[i][cls] = parseInt(val); break;
-        case "detune": nodeVals[i][cls] = parseInt(val); break;
-        case "wavetype": nodeVals[i][cls] = val; break;
-        case "real": nodeVals[i][cls] = nums(val); break;
-        case "imag": nodeVals[i][cls] = nums(val); break;
+        case "phase": notes[i][cls] = parseInt(val); break;
+        case "hz": notes[i][cls] = parseInt(val); break;
+        case "detune": notes[i][cls] = parseInt(val); break;
+        case "wavetype": notes[i][cls] = val; break;
+        case "real": notes[i][cls] = nums(val); break;
+        case "imag": notes[i][cls] = nums(val); break;
     }
-    node[i].stop()
-    node[i] = makeNodePlayer(nodeVals[i]);
+    notePlayers[i].stop()
+    notePlayers[i] = makeNotePlayer(notes[i]);
 }
