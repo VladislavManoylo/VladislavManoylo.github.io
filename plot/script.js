2b58e1c07a5a0f45413235cac8fc1f254f1a8282
vlad <vladislav.manoylo@gmail.com>
2024-11-26 19:49:54 -0500

    cleanup

--- plot/script.js
+++ plot/script.js
@@ -24,20 +24,20 @@ type Fun = { fun: "sin" | "cos" | "tan" | "ln"; arg: Expr };
 function coefLabel(coefficients) {
     return coefficients
         .map((v, i) => {
-        let base = v;
-        let exp = coefficients.length - 1 - i;
-        if (base === 0)
-            return "";
-        if (exp === 0)
-            return base.toString();
-        let ret = "";
-        if (base !== 1)
-            ret += base.toString();
-        ret += "x";
-        if (exp !== 1)
-            ret += `^${exp}`;
-        return ret;
-    })
+            let base = v;
+            let exp = coefficients.length - 1 - i;
+            if (base === 0)
+                return "";
+            if (exp === 0)
+                return base.toString();
+            let ret = "";
+            if (base !== 1)
+                ret += base.toString();
+            ret += "x";
+            if (exp !== 1)
+                ret += `^${exp}`;
+            return ret;
+        })
         .filter((s) => s.length > 0)
         .join(" + ");
 }
@@ -55,8 +55,8 @@ function coefToPolynomial(coefficients) {
     };
 }
 //console.log("139=", coefToPolynomial([1,10,100])(3));
-let canvas = document.getElementById("canvas");
-let pencil = new Pencil(canvas);
+const canvas = document.getElementById("canvas");
+const pencil = new Pencil(canvas);
 /** take samples of the function f between x0 and x1
  * should have at least 2 samples to work as expected
  */
@@ -70,44 +70,43 @@ function sampleFunction(f, x0, x1, samples) {
     }
     return ys;
 }
-let x0Input = document.getElementById("x0");
-let x1Input = document.getElementById("x1");
-let y0Input = document.getElementById("y0");
-let y1Input = document.getElementById("y1");
-let autorangeInput = document.getElementById("autorange");
-let funList = document.getElementById("funlist");
+const x0Input = document.getElementById("x0");
+const x1Input = document.getElementById("x1");
+const y0Input = document.getElementById("y0");
+const y1Input = document.getElementById("y1");
+const autorangeInput = document.getElementById("autorange");
+const funList = document.getElementById("funlist");
+
 function redraw() {
     let x0 = +x0Input.value;
     let x1 = +x1Input.value;
-    let paths = [];
-    for (let funInput of document.getElementsByClassName("fun")) {
-        let input = funInput;
+    const paths = [];
+    for (let input of document.getElementsByClassName("fun")) {
         let str = input.value;
         if (str !== "") {
             let coef = str.split(/\s+/).map(Number);
             input.labels[0].innerHTML = coefLabel(coef);
             paths.push(sampleFunction(coefToPolynomial(coef), x0, x1, 100));
-        }
-        else {
+        } else {
             input.labels[0].innerHTML = "";
         }
     }
     let y0 = +y0Input.value;
     let y1 = +y1Input.value;
     if (autorangeInput.checked) {
-        let ys = paths.flat();
+        const ys = paths.flat();
         y0 = Math.min(...ys);
         y1 = Math.max(...ys);
-        y0Input.value = y0.toString();
-        y1Input.value = y1.toString();
+        y0Input.value = y0.toPrecision(5);
+        y1Input.value = y1.toPrecision(5);
     }
     // plot paths
     pencil.clear();
-    let margin = 50;
+    const margin = 50;
     {
         // axes
-        let left = margin - 5;
-        let bottom = canvas.height - margin;
+        const left = margin - 5;
+        const bottom = canvas.height - margin;
         pencil.text(y1.toFixed(0), [left, 20], "1em Arial", "right");
         pencil.text(y0.toFixed(0), [left, bottom], "1em Arial", "right");
         bottom += 20;
@@ -117,7 +116,7 @@ function redraw() {
     pencil.ctx.translate(margin, canvas.height - margin);
     pencil.ctx.scale(1, -1);
     pencil.ctx.strokeRect(0, 0, canvas.width - margin, canvas.height - margin);
-    let height = y1 - y0;
+    const height = y1 - y0;
     if (height === 0) {
         let y = canvas.height / 2;
         pencil.path([
@@ -126,32 +125,22 @@ function redraw() {
         ]);
         return;
     }
-    let dy = (canvas.height - margin) / height;
-    for (let ys of paths) {
+    const dy = (canvas.height - margin) / height;
+    for (const ys of paths) {
         let dx = (canvas.width - margin) / (ys.length - 1);
         // max - y, because y=0 is the top of the canvas
         pencil.path(ys.map((y, x) => [dx * x, dy * (y - y0)]));
     }
 }
 redraw();
-x0Input.addEventListener("input", () => {
-    redraw();
-});
-x1Input.addEventListener("input", () => {
-    redraw();
-});
-y0Input.addEventListener("input", () => {
-    redraw();
-});
-y1Input.addEventListener("input", () => {
-    redraw();
-});
-autorangeInput.addEventListener("input", () => {
-    redraw();
-});
-funList.addEventListener("input", () => {
-    redraw();
-});
+
+x0Input.addEventListener("input", () => { redraw(); });
+x1Input.addEventListener("input", () => { redraw(); });
+y0Input.addEventListener("input", () => { redraw(); });
+y1Input.addEventListener("input", () => { redraw(); });
+autorangeInput.addEventListener("input", () => { redraw(); });
+funList.addEventListener("input", () => { redraw(); });
+
 document.getElementById("+").addEventListener("click", () => {
     let i = funList.childNodes.length;
     funList.insertAdjacentHTML("beforeend", `<li>
